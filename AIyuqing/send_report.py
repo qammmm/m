0240eccbbc21dfb271c5b484fb2d518715b7b816
mmm <97013884@qq.com>
2021-11-21 20:47:52 +0800

    11.21.20

--- AIyuqing/send_report.py
+++ AIyuqing/send_report.py
@@ -7,8 +7,7 @@ import requests
 class DingRobot:
     def __init__(self):
         self.allure = "http://mcm:mcm.521520.@47.94.5.28:9000/job/aiyuqing/allure/widgets/suites.json"
-        self.ding = 'https://oapi.dingtalk.com/robot/send?access_token=' \
-                    'ca6944d0d31cc88634a4b250e02d97ab3b94c48660bf7e82b0f809351a56cded'
+        self.ding = "https://oapi.dingtalk.com/robot/send?access_token=4047968f7f40495a86e51b1b4a8eb16a9fc806a005fb670a6476678d164661e7"
         self.error = self.get_allure()
 
     def get_allure(self):
@@ -20,15 +19,15 @@ class DingRobot:
         if self.error > 0:
             headers = {"Content-Type": "application/json;charset=utf-8"}
             content = {
-                "msgtype": "text",
-                "text": {
+                "msgtype": "link",
+                "link": {
                     "text": "账号mcm,密码mcm.521520.",
-                    "title": "铭大大" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    "title": "002" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                     "picUrl": "",
                     "messageUrl": "http://mcm:mcm.521520.@47.94.5.28:9000/job/aiyuqing/allure"
                 }
             }
-            response = requests.post(self.ding, headers=headers, data=json.dumps(content))
+            requests.post(self.ding, headers=headers, data=json.dumps(content))
         else:
             print('无报错')
 
